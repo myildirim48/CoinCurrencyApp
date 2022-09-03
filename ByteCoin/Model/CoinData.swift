263261af3d530b17ccdf1b1e1eefb78725ac53c3
myildirim48 <muhammedyildirim48@gmail.com>
2022-09-03 20:14:24 +0300

    Parse the JSON data

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by YILDIRIM on 3.09.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Codable{
+    let rate: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -9,11 +9,44 @@
 import Foundation
 struct CoinManager {
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "9A103275-2CCE-4AB9-BE7D-A9A72A3187F4"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getPrice(for currency: String){
+        
+        let urlString = "\(baseURL)/\(currency)/?apikey=\(apiKey)"
+        if let url = URL(string: urlString) {
+            
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    let bitcoinPrice = self.parseJSON(safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            
+            print(lastPrice)
+            
+            return lastPrice
+        }catch {
+            print(error)
+            return nil
+        }
         
     }
 }
