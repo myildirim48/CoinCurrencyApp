0b631e5eb7f7758e73d8d1c3621f7c6f78b3201a
myildirim48 <muhammedyildirim48@gmail.com>
2022-09-03 20:33:24 +0300

    Update User Interface, Refactor with extensions

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,12 +7,25 @@
 //
 
 import Foundation
+
+protocol CoinManagerDelegate {
+    
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+    
+}
+
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
+    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "9A103275-2CCE-4AB9-BE7D-A9A72A3187F4"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    
+    
     func getPrice(for currency: String){
         
         let urlString = "\(baseURL)/\(currency)/?apikey=\(apiKey)"
@@ -22,11 +35,15 @@ struct CoinManager {
             
             let task = session.dataTask(with: url) { data, response, error in
                 if error != nil {
-                    print(error!)
+                    self.delegate?.didFailWithError(error: error!)
                     return
                 }
                 if let safeData = data {
-                    let bitcoinPrice = self.parseJSON(safeData)
+                    if let bitcoinPrice = self.parseJSON(safeData){
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
                 }
             }
             task.resume()
